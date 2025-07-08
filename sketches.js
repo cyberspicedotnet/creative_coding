6c7fb9a5c393112bc6f87f146cbc8817d34a2e9c
cyberspice <domme.ewan@gmail.com>
2025-07-08 18:59:23 +0100

    Grid View of Sketches
    Chat GPT suggested way of displaying different sketches. Using sketches.js to keep an updated list of each sketch + a description

---
+++ sketches.js
@@ -0,0 +1,16 @@
+const sketches = [
+    {
+      title: "Bubble Class",
+      folder: "p5/bubble_class",
+      thumbnail: "sketches/sketch1/thumbnail.jpg",
+      alt: "Drag mouse to make bubbles! Using Classes",
+    },
+    {
+      title: "Example Coordinates",
+      folder: "p5/example_coordinates",
+      thumbnail: "sketches/sketch2/thumbnail.jpg",
+      alt: "Interactive demonstration of X,Y mouse coordinates",
+    },
+    // Add more sketches here
+  ];
+  
