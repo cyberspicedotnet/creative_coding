2e2f2c726acd713bd7ec919f4184c24e167d677d
cyberspice <domme.ewan@gmail.com>
2025-07-08 19:19:18 +0100

    Updating sketches.js list and thumbnails

--- sketches.js
+++ sketches.js
@@ -2,15 +2,56 @@ const sketches = [
     {
       title: "Bubble Class",
       folder: "p5/bubble_class",
-      thumbnail: "sketches/sketch1/thumbnail.jpg",
+      thumbnail: "p5/bubble_class/bubble_class_thumbnail.png",
       alt: "Drag mouse to make bubbles! Using Classes",
     },
     {
       title: "Example Coordinates",
       folder: "p5/example_coordinates",
-      thumbnail: "sketches/sketch2/thumbnail.jpg",
+      thumbnail: "p5/example_coordinates/example_coordinates_thumbnail.png",
       alt: "Interactive demonstration of X,Y mouse coordinates",
     },
-    // Add more sketches here
+    {
+      title: "Peach Grid",
+      folder: "p5/example_peach_grid",
+      thumbnail: "p5/example_peach_grid/example_peach_grid_thumbnail.png",
+      alt: "Generative grid with peach-colored palette",
+    },
+    {
+      title: "Random Lines",
+      folder: "p5/example_random_lines",
+      thumbnail: "p5/example_random_lines/example_random_lines_thumbnail.png",
+      alt: "Randomly placed lines across the canvas",
+    },
+    {
+      title: "Sine Wave",
+      folder: "p5/example_sinewave",
+      thumbnail: "p5/example_sinewave/example_sinewave_thumbnail.png",
+      alt: "Animated sine wave visual",
+    },
+    {
+      title: "Genuary 25: Isometric",
+      folder: "p5/genuary_25_iso",
+      thumbnail: "p5/genuary_25_iso/genuary_25_iso_thumbnail.png",
+      alt: "Isometric artwork for Genuary 25",
+    },
+    {
+      title: "Genuary 25: Lines",
+      folder: "p5/genuary_25_lines",
+      thumbnail: "p5/genuary_25_lines/genuary_25_lines_thumbnail.png",
+      alt: "Line-based artwork for Genuary 25",
+    },
+    {
+      title: "New News",
+      folder: "p5/new_news",
+      thumbnail: "p5/new_news/new_news_thumbnail.png",
+      alt: "Generative visualization based on headlines",
+    },
+    {
+      title: "Touch Bubble",
+      folder: "p5/touch_bubble",
+      thumbnail: "p5/touch_bubble/touch_bubble_thumbnail.png",
+      alt: "Touch interactive bubble sketch",
+    },
   ];
   
